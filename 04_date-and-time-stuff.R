87d90efc7d9605ac3b4d11a28034050fea17ca3c
Adam Davis <davisadamw@gmail.com>
2019-08-06 16:38:13 -0700

    grabbed time by two separate methods

--- 04_date-and-time-stuff.R
+++ 04_date-and-time-stuff.R
@@ -8,16 +8,21 @@ shows_allcols <- read_csv('shows_allcols.csv')
 
 
 # get a bit more of ~ start/end time info
-shows_showtime <- shows_allcols %>% 
+shows_alltimes <- shows_allcols %>% 
   separate(time, c('start_time', 'end_time'), sep = '-', remove = FALSE) %>% 
   mutate(end_time = str_replace(end_time, ' PM', ''),
          date_dmy = lubridate::parse_date_time(date, '%A, %b. %d')) %>% 
-  mutate_at(vars(ends_with('_time')), ~ time_from_noon(., date_dmy)) %>% 
-  mutate(show_interval = interval(start_time, end_time))
-            
-         #    
-         #    ,
-         # start_time_l = if_else(hm(start_time),
-         # end_time_l   = hm(end_time),
-         # start_dt = date_dmy + hours(12) + start_time_l)
-         # 
+  mutate_at(vars(ends_with('_time')), 
+            list(to = hm_safe,
+                 dt = ~ time_from_noon(., date_dmy))) %>% 
+  mutate(show_interval = interval(start_time_dt, end_time_dt))
+
+# write to disk
+shows_alltimes %>% 
+  write_rds('shows_all_time_data.rds')
+
+
+
+
+
+
