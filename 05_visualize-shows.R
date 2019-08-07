6e8060780a75b69da70650ceb3586f1273d67717
Adam Davis <davisadamw@gmail.com>
2019-08-06 18:11:09 -0700

    recs and stuff

--- 05_visualize-shows.R
+++ 05_visualize-shows.R
@@ -5,9 +5,26 @@ source('99_freaky-time-function.R')
 # read show info with all time columns
 shows_alltimes <- read_rds('shows_all_time_data.rds')
 
+# load who we wanna see
+wwws <- readxl::read_excel('who-we-wanna-see.xlsx') %>%
+  select(artist, ymn:comments) %>% 
+  mutate(`See Em` = if_else(is.na(ymn), 'Unk', ymn) %>% 
+           fct_relevel('Unk', 'No', 'Maybe', 'Yes'),
+         see_em_size = case_when(ymn == 'No'    ~ 0.5,
+                                 ymn == 'Maybe' ~ 1,
+                                 ymn == 'Yes'   ~ 1,
+                                 TRUE           ~ 0.5))
+
+
+color_scale = c('Yes'   = 'Green',
+                'Maybe' = 'Yellow',
+                'No'    = 'Red',
+                'Unk'   = '#505050')
+
 # long format, only cols we want
 shows_long <- shows_alltimes %>% 
   select(artist, stage, date, start_time, end_time) %>% 
+  left_join(wwws, by = 'artist') %>% 
   gather('set', 'time', ends_with('time')) %>% 
   mutate(time_hm  = hm_safe(time) + hours(12),
          time_dhm = time_from_noon(time, lubridate::parse_date_time(date, '%A, %b. %d')),
@@ -17,9 +34,15 @@ shows_long <- shows_alltimes %>%
 
 # plot with path
 shows_long %>%
-  ggplot(aes(x = time_hm, y = Stage, group = artist)) +
-  scale_x_time(labels = lt24) + 
+  ggplot(aes(x = time_hm, y = Stage, 
+             group = artist, color = `See Em`, size = 4*see_em_size)) +
+  scale_x_time(labels = lt12p) + 
+  scale_size_identity() +
+  scale_color_manual(values = color_scale, guide = 'none') +
   facet_grid(rows = vars(date), scales = 'free_x') +
   geom_path() +
-  theme(axis.title.x = element_blank())
+  theme_bw() +
+  theme(axis.title = element_blank(),
+        axis.text = element_text(size = 15))
 
+ggsave('t1.png')
