99b08cf384e863621a61bf1ef88e1e751a0ed087
Adam Davis <davisadamw@gmail.com>
2019-08-07 18:19:09 -0700

    set overwrite to true for the data pull

--- 05_pull-wants-googlesheets.R
+++ 05_pull-wants-googlesheets.R
@@ -7,5 +7,5 @@ osl_sheet_location <- drive_find(pattern = 'OSL', type = 'spreadsheet')
 osl_to_watch <- drive_get(as_id(osl_sheet_location$id))
 
 # download it
-drive_download(osl_to_watch, type = 'xlsx')
+drive_download(osl_to_watch, type = 'xlsx', overwrite = TRUE)
 
