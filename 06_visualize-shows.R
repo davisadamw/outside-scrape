ae4e4b7dd87e0a7b6f6158d0460b7e32f5a7be33
Adam Davis <davisadamw@gmail.com>
2019-08-08 22:58:53 -0700

    Updated figures and figure code with text for acts and output that works well on phone screen

--- 06_visualize-shows.R
+++ 06_visualize-shows.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(googledrive)
 
 source('99_freaky-time-function.R')
 
@@ -8,7 +7,7 @@ shows_alltimes <- read_rds('shows_all_time_data.rds')
 
 # load who we wanna see
 wwws <- readxl::read_excel('OSL-2019.xlsx') %>%
-  select(artist, ymn:comments) %>% 
+  select(artist, artist_simp, ymn:comments) %>% 
   mutate(`See Em` = if_else(is.na(ymn), 'Unk', ymn) %>% 
            fct_relevel('Unk', 'No', 'Maybe', 'Yes'),
          see_em_size = case_when(ymn == 'No'    ~ 0.5,
@@ -27,23 +26,97 @@ shows_long <- shows_alltimes %>%
   select(artist, stage, date, start_time, end_time) %>% 
   left_join(wwws, by = 'artist') %>% 
   gather('set', 'time', ends_with('time')) %>% 
-  mutate(time_hm  = hm_safe(time) + hours(12),
-         time_dhm = time_from_noon(time, lubridate::parse_date_time(date, '%A, %b. %d')),
+  mutate(time_hm  = hm_safe(time),
+         time_minute = 60 * hour(time_hm) + minute(time_hm),
          Stage = str_replace(stage, ' Stage', '') %>% 
            fct_relevel('GastroMagic', 'Sutro', 'Lands End', 'Panhandle', 'Twin Peaks')) 
 
+# and the text positioning
+shows_text <- shows_long %>% 
+  filter(`See Em` == 'Yes' | `See Em` == 'Maybe') %>% 
+  select(artist, artist_simp, Stage, `See Em`, date, time_minute) %>% 
+  group_by(artist, artist_simp, Stage, `See Em`, date) %>% 
+  summarise(time_minute = mean(time_minute))
 
 # plot with path
 shows_long %>%
-  ggplot(aes(x = time_hm, y = Stage, 
+  ggplot(aes(x = time_minute, y = Stage, 
              group = artist, color = `See Em`, size = 4*see_em_size)) +
-  scale_x_time(labels = lt12p) + 
+  scale_x_continuous(breaks = seq(0, 600, 60), minor_breaks = seq(0, 600, 20),
+                     limits = c(-10, 610), expand = c(0,0),
+                     labels = c('Noon', paste(seq(1,10), '00', sep= ':'))) + 
   scale_size_identity() +
   scale_color_manual(values = color_scale, guide = 'none') +
   facet_grid(rows = vars(date), scales = 'free') +
   geom_path() +
+  geom_text(aes(label=artist_simp), data = shows_text,
+            color = 'black', size = 3, nudge_y = 0.3) +
   theme_bw() +
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 15))
+        axis.text.y = element_text(size = 11, angle = 30),
+        axis.text.x = element_text(size = 11))
 
-ggsave('OSL_outline.png')
+ggsave('OSL_outline.png', width=7, height=7)
+
+# plot with path FRIDAY
+shows_long %>%
+  filter(str_detect(date, 'Fri')) %>% 
+  ggplot(aes(x = time_minute, y = Stage, 
+             group = artist, color = `See Em`, size = 4*see_em_size)) +
+  scale_x_continuous(breaks = seq(0, 600, 60), minor_breaks = seq(0, 600, 20),
+                     limits = c(-10, 610), expand = c(0,0),
+                     labels = c('Noon', paste(seq(1,10), '00', sep= ':'))) + 
+  scale_size_identity() +
+  scale_color_manual(values = color_scale, guide = 'none') +
+  facet_grid(rows = vars(date), scales = 'free') +
+  geom_path() +
+  geom_text(aes(label=artist_simp), data = filter(shows_text, str_detect(date, 'Fri')),
+            color = 'black', size = 3, nudge_y = 0.3) +
+  theme_bw() +
+  theme(axis.title = element_blank(),
+        axis.text.y = element_text(size = 11, angle = 30),
+        axis.text.x = element_text(size = 11))
+
+ggsave('OSL_friday.png', width=7, height=3)
+
+# plot with path FRIDAY
+shows_long %>%
+  filter(str_detect(date, 'Sat')) %>% 
+  ggplot(aes(x = time_minute, y = Stage, 
+             group = artist, color = `See Em`, size = 4*see_em_size)) +
+  scale_x_continuous(breaks = seq(0, 600, 60), minor_breaks = seq(0, 600, 20),
+                     limits = c(-10, 610), expand = c(0,0),
+                     labels = c('Noon', paste(seq(1,10), '00', sep= ':'))) + 
+  scale_size_identity() +
+  scale_color_manual(values = color_scale, guide = 'none') +
+  facet_grid(rows = vars(date), scales = 'free') +
+  geom_path() +
+  geom_text(aes(label=artist_simp), data = filter(shows_text, str_detect(date, 'Sat')),
+            color = 'black', size = 3, nudge_y = 0.3) +
+  theme_bw() +
+  theme(axis.title = element_blank(),
+        axis.text.y = element_text(size = 11, angle = 30),
+        axis.text.x = element_text(size = 11))
+
+ggsave('OSL_Saturday.png', width=7, height=3)
+
+# plot with path FRIDAY
+shows_long %>%
+  filter(str_detect(date, 'Sun')) %>% 
+  ggplot(aes(x = time_minute, y = Stage, 
+             group = artist, color = `See Em`, size = 4*see_em_size)) +
+  scale_x_continuous(breaks = seq(0, 600, 60), minor_breaks = seq(0, 600, 20),
+                     limits = c(-10, 610), expand = c(0,0),
+                     labels = c('Noon', paste(seq(1,10), '00', sep= ':'))) + 
+  scale_size_identity() +
+  scale_color_manual(values = color_scale, guide = 'none') +
+  facet_grid(rows = vars(date), scales = 'free') +
+  geom_path() +
+  geom_text(aes(label=artist_simp), data = filter(shows_text, str_detect(date, 'Sun')),
+            color = 'black', size = 3, nudge_y = 0.3) +
+  theme_bw() +
+  theme(axis.title = element_blank(),
+        axis.text.y = element_text(size = 11, angle = 30),
+        axis.text.x = element_text(size = 11))
+
+ggsave('OSL_Sunday.png', width=7, height=3)
